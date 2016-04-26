63ab6ef2a530f127b8ea9bdf12c34b857d2ad33f
Eric McCorkle <emccorkle@codiscope.com>
2016-04-25 22:52:13 -0400

    Added PositionElement typeclass

---
+++ src/Data/PositionElement.hs
@@ -0,0 +1,90 @@
+-- Copyright (c) 2015 Eric McCorkle.  All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+--
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+--
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- 3. Neither the name of the author nor the names of any contributors
+--    may be used to endorse or promote products derived from this software
+--    without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+-- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+-- PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS
+-- OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+-- LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+-- USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+-- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+{-# OPTIONS_GHC -funbox-strict-fields -Wall -Werror #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses,
+             FunctionalDependencies #-}
+
+module Data.PositionElement(
+       PositionElement(..)
+       ) where
+
+import Data.Position.BasicPosition(BasicPosition)
+import Data.Position.DWARFPosition(DWARFPosition)
+
+import qualified Data.Position.BasicPosition as Basic
+import qualified Data.Position.DWARFPosition as DWARF
+
+-- | Typeclass of data with a notion of position.
+class PositionElement defid tydefid ty | ty -> defid, ty -> tydefid where
+  -- | Get basic position data.
+  position :: ty -> BasicPosition
+  position e =
+    case debugPosition e of
+      DWARF.Def { DWARF.defPos = DWARF.Span { DWARF.spanStart = start,
+                                              DWARF.spanEnd = end } } ->
+        Basic.Span { Basic.spanStart = start, Basic.spanEnd = end }
+      DWARF.Def { DWARF.defPos = DWARF.Point { DWARF.pointPos = pos } } ->
+        Basic.Point { Basic.pointPos = pos }
+      DWARF.TypeDef { DWARF.typeDefPos = DWARF.Span { DWARF.spanStart = start,
+                                                      DWARF.spanEnd = end } } ->
+        Basic.Span { Basic.spanStart = start, Basic.spanEnd = end }
+      DWARF.TypeDef {
+        DWARF.typeDefPos = DWARF.Point { DWARF.pointPos = pos }
+      } -> Basic.Point { Basic.pointPos = pos }
+      DWARF.Block { DWARF.blockPos = DWARF.Span { DWARF.spanStart = start,
+                                                  DWARF.spanEnd = end } } ->
+        Basic.Span { Basic.spanStart = start, Basic.spanEnd = end }
+      DWARF.Block { DWARF.blockPos = DWARF.Point { DWARF.pointPos = pos } } ->
+        Basic.Point { Basic.pointPos = pos }
+      DWARF.Simple { DWARF.simplePos = DWARF.Span { DWARF.spanStart = start,
+                                                    DWARF.spanEnd = end } } ->
+        Basic.Span { Basic.spanStart = start, Basic.spanEnd = end }
+      DWARF.Simple { DWARF.simplePos = DWARF.Point { DWARF.pointPos = pos } } ->
+        Basic.Point { Basic.pointPos = pos }
+      DWARF.File { DWARF.fileName = fname } ->
+        Basic.File { Basic.fileName = fname }
+      DWARF.Synthetic { DWARF.synthDesc = desc } ->
+        Basic.Synthetic { Basic.synthDesc = desc }
+      DWARF.CmdLine -> Basic.CmdLine
+
+  -- | Get full debugging position data.
+  debugPosition :: ty -> DWARFPosition defid tydefid
+  debugPosition e =
+    case position e of
+      Basic.Span { Basic.spanStart = start, Basic.spanEnd = end } ->
+        DWARF.Simple { DWARF.simplePos = DWARF.Span { DWARF.spanStart = start,
+                                                      DWARF.spanEnd = end } }
+      Basic.Point { Basic.pointPos = pos } ->
+        DWARF.Simple { DWARF.simplePos = DWARF.Point { DWARF.pointPos = pos } }
+      Basic.File { Basic.fileName = fname } ->
+        DWARF.File { DWARF.fileName = fname }
+      Basic.Synthetic { Basic.synthDesc = desc } ->
+        DWARF.Synthetic { DWARF.synthDesc = desc }
+      Basic.CmdLine -> DWARF.CmdLine
